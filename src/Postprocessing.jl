56aaaea874f38c8fe6825b1b3497a183db6cd8ec
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2025-02-13 15:00:07 +0100

    outsoruce plotting functions

--- src/Postprocessing.jl
+++ src/Postprocessing.jl
@@ -3,11 +3,150 @@ using Gridap.CellData
 using Gridap.Arrays
 using CairoMakie
 using LineSearches
+using Colors
 
 using Random
 Random.seed!(1234)
 
-export plot_all_unitsquare
+export plot_all_unitsquare, contourplot_unitsquare, contourplot_coannulus,
+       cmap_cold_to_hot_paraview
+
+function cmap_cold_to_hot_paraview()
+  edges = [
+    0.0, 0.45, 0.5, 0.55, 1.0
+  ]
+  colors = [
+    RGBA(0, 1, 1, 1.0), RGBA(0, 0, 1, 1.0), RGBA(0, 0, 0.5019608, 1.0),
+    RGBA(1, 0, 0, 1.0), RGBA(1, 1, 0, 1.0)
+  ]
+  return cgrad(colors, edges)
+end
+
+function contourplot_coannulus2(;
+  fun::FEFunction=Gridap.interpolate(
+    x->x[1]+x[2],
+    TestFESpace(
+      CartesianDiscreteModel((-2,2,-2,2),(10,10)),
+      ReferenceFE(lagrangian, Float64, 2), conformity=:H1
+    )
+  ),
+  name="out",
+  ri = 2/3,
+  ro = 5/3,
+  eps = 0.01,
+  n_plot=200,
+  axisargs=(;),
+  contourargs=(;),
+  surfaceargs=(;),
+  withsurface=true,
+)
+
+  rs = LinRange(ri+eps, ro-eps, n_plot)
+  phis = LinRange(0, 2*pi, 2*n_plot-1)
+
+  search_method = KDTreeSearch(num_nearest_vertices=5)
+  fun_int = Interpolable(fun; searchmethod=search_method)
+  cache = return_cache(fun_int, Gridap.Point(1.0, 1.0))
+  function fun_help(x)
+    return evaluate!(cache, fun_int, Gridap.Point(x))
+  end
+  xs = [r * cos(phi) for r in rs, phi in phis]
+  ys = [r * sin(phi) for r in rs, phi in phis]
+  zs = fun_help.(broadcast((x, y) -> (x, y), xs, ys))
+  f = Figure(
+    size=(500, 500), figure_padding=(0, 10, 0, 0)
+  )
+  Axis(
+    f[1, 1],
+    aspect=1,
+    # title="stream function ψ",
+    xlabel="x",
+    ylabel="y",
+    xminorticksvisible=true,
+    yminorticksvisible=true,
+    xticks=LinearTicks(6),
+    yticks=LinearTicks(6),
+    limits=(-ro, ro, -ro, ro);
+    axisargs...
+  )
+  @show extrema(zs)
+  if withsurface
+    surface!(
+      xs, ys, zs, shading = NoShading, colormap = :coolwarm, rasterize = true;
+      surfaceargs...
+    )
+  end
+
+  contour!(
+    xs, ys, zs, levels = 10, labels=true, labelsize=15, color=:black;
+    contourargs...
+  )
+  arc!(Point2f(0), ro, -π, π, color = :black, linewidth = 2)
+  arc!(Point2f(0), ri, -π, π, color = :black, linewidth = 2)
+  save("$(name).pdf", f)
+  return f
+end
+contourplot_coannulus=contourplot_coannulus2
+
+
+function contourplot_unitsquare2(;
+  fun::FEFunction=Gridap.interpolate(
+    x->x[1]+x[2],
+    TestFESpace(
+      CartesianDiscreteModel((0,1,0,1),(10,10)),
+      ReferenceFE(lagrangian, Float64, 2), conformity=:H1
+    )
+  ),
+  name="out",
+  n_plot=100,
+  axisargs=(;),
+  contourargs=(;),
+  surfaceargs=(;),
+  withsurface=true,
+)
+  xs = LinRange(0, 1, n_plot)
+  ys = LinRange(0, 1, n_plot)
+  search_method = KDTreeSearch(num_nearest_vertices=10)
+
+  f = Figure(
+    size=(500, 500), figure_padding=(0, 10, 0, 0)
+  )
+  Axis(
+    f[1, 1],
+    aspect=1,
+    # title="stream function ψ",
+    xlabel="x",
+    ylabel="y",
+    xminorticksvisible=true,
+    yminorticksvisible=true,
+    xticks=LinearTicks(6),
+    yticks=LinearTicks(6),
+    limits=(0, 1, 0, 1);
+    axisargs...
+  )
+  fun_int = Interpolable(fun; searchmethod=search_method)
+  cache = return_cache(fun_int, Gridap.Point(0.0, 0.0))
+  function fun_help(x)
+    return evaluate!(cache, fun_int, Gridap.Point(x))
+  end
+  zs = [fun_help([x, y]) for x in xs, y in ys]
+  @show extrema(zs)
+  if withsurface
+    surface!(
+      xs, ys, zs, shading = NoShading, colormap = :coolwarm, rasterize = true;
+      surfaceargs...
+    )
+  end
+  contour!(
+    xs, ys, zs, levels=10, labels=true, labelsize=15, color=:black,
+    colormap = :coolwarm;
+    contourargs...
+  )
+  save("$(name).pdf", f)
+  return f
+end
+contourplot_unitsquare=contourplot_unitsquare2
+
 
 """
     plot_all_unitsquare2(psih, Th, uh, model, name, levels)
